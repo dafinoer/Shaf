4c68f1394ef9d8e7ea207bd49733b3748be6de36
dafinoer <dafinoer@outlook.com>
2023-11-26 21:47:14 +0700

    add color

--- app/src/main/java/com/dafinrs/shaf/android/ui/theme/Color.kt
+++ app/src/main/java/com/dafinrs/shaf/android/ui/theme/Color.kt
@@ -21,47 +21,47 @@ data class ColorSchemeShaf(
     val onSurfaceVariant: Color,
     val outline: Color,
     val outlineVariant: Color,
-)
-
-fun ColorSchemeShafLight(): ColorSchemeShaf = ColorSchemeShaf(
-    primary = Color(0xff006C50),
-    onPrimary = Color.White,
-    primaryContainer = Color(0xff7FF9CB),
-    onPrimaryContainer = Color(0xff002116),
-    secondary = Color(0xff4C6359),
-    onSecondary = Color.White,
-    secondaryContainer = Color(0xffCEE9DB),
-    onSecondaryContainer = Color(0xff092017),
-    tertiary = Color(0xff3F6375),
-    onTertiary = Color.White,
-    tertiaryContainer = Color(0xffC2E8FD),
-    onTertiaryContainer = Color(0xff001F2A),
-    surface = Color(0xffF8FAF6),
-    onSurface = Color(0xff191C1A),
-    surfaceVariant = Color(0xffDBE5DE),
-    onSurfaceVariant = Color(0xff404944),
-    outline = Color(0xff707974),
-    outlineVariant = Color(0xffBFC9C2)
-)
-
-fun ColorSchemeShafDark(): ColorSchemeShaf = ColorSchemeShaf(
-    primary = Color(0xff61DCB0),
-    onPrimary = Color(0xff003828),
-    primaryContainer = Color(0xff00513C),
-    onPrimaryContainer = Color(0xff7FF9CB),
-    secondary = Color(0xffB3CCBF),
-    onSecondary = Color(0xff1E352C),
-    secondaryContainer = Color(0xff354B41),
-    onSecondaryContainer = Color(0xffCEE9DB),
-    tertiary = Color(0xffA6CCE0),
-    onTertiary = Color(0xff0A3545),
-    tertiaryContainer = Color(0xff264B5C),
-    onTertiaryContainer = Color(0xffC2E8FD),
-    surface = Color(0xff111412),
-    onSurface = Color(0xffC5C7C4),
-    surfaceVariant = Color(0xff404944),
-    onSurfaceVariant = Color(0xffBFC9C2),
-    outline = Color(0xff89938D),
-    outlineVariant = Color(0xff404944),
-)
-
+) {
+    companion object {
+        val light = ColorSchemeShaf(
+            primary = Color(0xff006C50),
+            onPrimary = Color.White,
+            primaryContainer = Color(0xff7FF9CB),
+            onPrimaryContainer = Color(0xff002116),
+            secondary = Color(0xff4C6359),
+            onSecondary = Color.White,
+            secondaryContainer = Color(0xffCEE9DB),
+            onSecondaryContainer = Color(0xff092017),
+            tertiary = Color(0xff3F6375),
+            onTertiary = Color.White,
+            tertiaryContainer = Color(0xffC2E8FD),
+            onTertiaryContainer = Color(0xff001F2A),
+            surface = Color(0xffF8FAF6),
+            onSurface = Color(0xff191C1A),
+            surfaceVariant = Color(0xffDBE5DE),
+            onSurfaceVariant = Color(0xff404944),
+            outline = Color(0xff707974),
+            outlineVariant = Color(0xffBFC9C2)
+        )
+        val dark = ColorSchemeShaf(
+            primary = Color(0xff61DCB0),
+            onPrimary = Color(0xff003828),
+            primaryContainer = Color(0xff00513C),
+            onPrimaryContainer = Color(0xff7FF9CB),
+            secondary = Color(0xffB3CCBF),
+            onSecondary = Color(0xff1E352C),
+            secondaryContainer = Color(0xff354B41),
+            onSecondaryContainer = Color(0xffCEE9DB),
+            tertiary = Color(0xffA6CCE0),
+            onTertiary = Color(0xff0A3545),
+            tertiaryContainer = Color(0xff264B5C),
+            onTertiaryContainer = Color(0xffC2E8FD),
+            surface = Color(0xff111412),
+            onSurface = Color(0xffC5C7C4),
+            surfaceVariant = Color(0xff404944),
+            onSurfaceVariant = Color(0xffBFC9C2),
+            outline = Color(0xff89938D),
+            outlineVariant = Color(0xff404944),
+        )
+    }
+}
