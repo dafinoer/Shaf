5627b68477bc5ede66a9b205e992b087fd49949e
dafinoer <dafinoer@outlook.com>
2023-12-07 11:38:45 +0700

    refactor page

--- app/src/main/java/com/dafinrs/shaf/android/pages/dashboard/DashboardScreenPage.kt
+++ app/src/main/java/com/dafinrs/shaf/android/pages/dashboard/DashboardScreenPage.kt
@@ -1,23 +1,22 @@
 package com.dafinrs.shaf.android.pages.dashboard
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import com.dafinrs.shaf.android.pages.home.HomePage
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DashboardScreenPage() {
-    Scaffold {
-        Box(modifier = Modifier.padding(it)) {
-            Column {
-                HomePage()
-            }
-        }
+fun DashboardScreenPage(navController: NavController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        HomePage(navController)
     }
 }
