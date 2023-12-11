9a30433bf5659805da36f69bccf03bbeb5b9f918
dafinoer <dafinoer@outlook.com>
2023-12-11 13:36:04 +0700

    refactor code

--- app/src/main/java/com/dafinrs/shaf/android/pages/home/components/ContentComponent.kt
+++ app/src/main/java/com/dafinrs/shaf/android/pages/home/components/ContentComponent.kt
@@ -1,8 +1,10 @@
 package com.dafinrs.shaf.android.pages.home.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,9 +14,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 @Composable
-fun ContentComponent(modifier: Modifier = Modifier) {
+fun ContentComponent(modifier: Modifier = Modifier, navController: NavController) {
     Box(
         modifier = modifier
             .fillMaxWidth()
@@ -23,31 +26,43 @@ fun ContentComponent(modifier: Modifier = Modifier) {
             .background(MaterialTheme.colorScheme.surface),
         contentAlignment = Alignment.BottomCenter
     ) {
-        LazyColumn {
+        LazyColumn(verticalArrangement = Arrangement.Top, modifier = Modifier.fillMaxSize()) {
             item {
                 TabBarComponent()
             }
 
             item {
-                TileComponent(fromTime = "12:00", toTime = "13:00")
+                TileComponent(fromTime = "12:00", toTime = "13:00") {
+                    navController.navigate("detail/1")
+                }
             }
 
             item {
-                TileComponent(fromTime = "12:00", toTime = "13:00")
+                TileComponent(fromTime = "12:00", toTime = "13:00") {
+                    navController.navigate("detail/1")
+                }
             }
 
             item {
-                TileComponent(fromTime = "12:00", toTime = "13:00")
+                TileComponent(fromTime = "12:00", toTime = "13:00") {
+                    navController.navigate("detail/1")
+                }
             }
 
             item {
-                TileComponent(fromTime = "12:00", toTime = "13:00")
+                TileComponent(fromTime = "12:00", toTime = "13:00"){
+                    navController.navigate("detail/1")
+                }
             }
             item {
-                TileComponent(fromTime = "12:00", toTime = "13:00")
+                TileComponent(fromTime = "12:00", toTime = "13:00"){
+                    navController.navigate("detail/1")
+                }
             }
             item {
-                TileComponent(fromTime = "12:00", toTime = "13:00")
+                TileComponent(fromTime = "12:00", toTime = "13:00"){
+                    navController.navigate("detail/1")
+                }
             }
         }
     }
